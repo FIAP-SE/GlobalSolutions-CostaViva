50cce74f7ede8d55bfde2d2b4f0ba5a1cebc56db
andré <andr3.luiz.viana@gmail.com>
2024-06-07 20:37:55 -0300

    feat: inclusão do vídeo pitch no projeto

--- src/routes/AboutProject/Home/index.tsx
+++ src/routes/AboutProject/Home/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
                     <iframe
                         width="560"
                         height="315"
-                        src="https://www.youtube.com/embed/yrBm3DRIoLE?si=AFyoufVDRTGAUKwW"
+                        src="https://www.youtube.com/embed/U6LOibqHfko"
                         title="YouTube video player"
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
